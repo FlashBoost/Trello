b3d4cca3448e789752c712034e5ffdc2b50a1763
FlashBoost <1060629887@qq.com>
2021-04-22 18:39:34 +0800

    work

---
+++ fontend/trello/src/views/Login.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="continer">
+
+  </div>
+</template>
---
+++ fontend/trello/vue.config.ts
@@ -0,0 +1,9 @@
+import path = require("path")
+
+function resolve(url: string): string {
+  return path.resolve(__dirname, url)
+}
+
+module.exports = {
+
+}
